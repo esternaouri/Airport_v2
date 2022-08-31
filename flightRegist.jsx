b1c897c0ba43d8ca9b635a4ad72092c677db1aa1
esternaouri <45138698+esternaouri@users.noreply.github.com>
2022-08-31 21:17:45 +0300

    Add files via upload

--- flightRegist.jsx
+++ flightRegist.jsx
@@ -5,16 +5,18 @@ import { Row, Col } from "antd";
 import UserTable from "./UserTable";
 import AddUserForm from "./AddUser";
 import Formy from "./form";
-import Products from"./form2";
+import Flights from"./form2";
 
 const Fr=()=>{
 return(
-<div>
-      <div class=" ontainer-sm card-text  bg-success" ><p style={{color:"white" ,textAlign:"center"}}>הטיסות נשלחות למודיעין הטיס, מייל עם אישור טיסה ישלח אוטומטי . יש לשמור ברקוד לביקרות</p></div>
-<Products></Products> 
-<br></br>   
+<div style={{width:"100%"
+}}>
+<h1 style={{color:"white",backgroundColor:"black", textAlign:"center"}}> 🛂 שידור טיסות למודיעין הטייס</h1>
+<div style={{display:"flex", flexWrap:"wrap"}}> <Flights></Flights> </div>
+
+<h2 style={{ textAlign:"center"}}>🚁</h2>
 <br></br>
-<div class=" ontainer-sm card-text  bg-dark" ><p style={{color:"white" ,textAlign:"center"}}>טיסות בטוחות</p></div>
+<div class=" ontainer-sm card-text  bg-success" ><p style={{color:"white" ,textAlign:"center"}}>הטיסות נשלחות למודיעין הטיס, מייל עם אישור טיסה ישלח אוטומטי . יש לשמור ברקוד לביקרות</p></div>
 
     </div>
   );
