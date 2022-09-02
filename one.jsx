97278ed03db9d665292249279593520341da200c
esternaouri <45138698+esternaouri@users.noreply.github.com>
2022-09-02 16:40:20 +0300

    Add files via upload

--- one.jsx
+++ one.jsx
@@ -10,26 +10,26 @@ const One =()=>{
      <div class="card mb-3">
   <div class="card-body">
     <h5 class="card-title">LLHA - A.I.P / חיפה</h5>
-    <p class="card-text">שדה תעופה חיפה הוא צפוני, בעיר חיפה, הוא שדה תעופה בנלאומי- <br></br><a href="https://www.gov.il/BlobFolder/generalpage/haifa-airport/he/aip_haifa-llha.pdf">aip- haifa</a></p>
+    <p class="card-text">שדה תעופה חיפה הוא צפוני, בעיר חיפה, הוא שדה תעופה בנלאומי- <br></br><a href="https://www.gov.il/BlobFolder/generalpage/haifa-airport/he/aip_haifa-llha.pdf" target="_blank">aip- haifa</a></p>
   </div>
 
 </div>
 <div class="card mb-3">
   <div class="card-body">
     <h5 class="card-title">LLHZ - A.I.P / הרצליה</h5>
-    <p class="card-text">שדה תעופה באיזור רעננה והרצליה, מיועד לאימוני טייסים צעירים ולטיסות  פנימיות <br></br><a href="https://www.gov.il/BlobFolder/generalpage/herzelia-airport/he/aip_herzliya-llhz1.pdf">aip- Herzzelya</a></p>
+    <p class="card-text">שדה תעופה באיזור רעננה והרצליה, מיועד לאימוני טייסים צעירים ולטיסות  פנימיות <br></br><a href="https://www.gov.il/BlobFolder/generalpage/herzelia-airport/he/aip_herzliya-llhz1.pdf"  target="_blank">aip- Herzzelya</a></p>
   </div>
 
 </div>     <div class="card mb-3">
   <div class="card-body">
     <h5 class="card-title">LLBG - A.I.P/ בן גוריון </h5>
-    <p class="card-text">שדה תעופה בן גוריון הוא שדה תעופה מרכזי בישראל  ושדה תעופה בנלאומי<br></br><a href="https://www.gov.il/en/Departments/General/electronic-aip">aip- Ben Gurion</a></p>
+    <p class="card-text">שדה תעופה בן גוריון הוא שדה תעופה מרכזי בישראל  ושדה תעופה בנלאומי<br></br><a href="https://www.gov.il/en/Departments/General/electronic-aip" target="_blank">aip- Ben Gurion</a></p>
   </div>
 
 </div>     <div class="card mb-3">
   <div class="card-body">
     <h5 class="card-title">LLIB - A.I.P/ ראש פינה</h5>
-    <p class="card-text">שדה תעופה ראש פינה הוא שדה צפוני וקרוב לכנרת  - <br></br><a href="https://www.gov.il/BlobFolder/generalpage/roashpina-airport/he/aip_roshpina-llib.pdf">aip- Rosh Pina</a></p>
+    <p class="card-text">שדה תעופה ראש פינה הוא שדה צפוני וקרוב לכנרת  - <br></br><a href="https://www.gov.il/BlobFolder/generalpage/roashpina-airport/he/aip_roshpina-llib.pdf" target="_blank">aip- Rosh Pina</a></p>
   </div>
 
 </div>
